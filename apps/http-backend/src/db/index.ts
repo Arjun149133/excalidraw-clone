ff578f7e592dc590df7e9f50fcb9f6de7163dd17
Arjun Yadav <arjun149133@gmail.com>
2025-01-12 14:03:47 +0530

    db

---
+++ apps/http-backend/src/db/index.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient();
+
+export default client;
--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,23 @@
 import express from "express";
+import db from "./db";
 
 const app = express();
+
+app.get("/create-user", async (req, res) => {
+  const random = Math.floor(Math.random() * 100);
+
+  await db.user.create({
+    data: {
+      name: "arjun@" + random,
+    },
+  });
+
+  const users = await db.user.findMany();
+
+  res.json({
+    users: users,
+  });
+});
 
 app.listen(5000, () => {
   console.log("http-backend on port 5000");
