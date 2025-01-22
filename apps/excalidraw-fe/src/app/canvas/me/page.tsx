1a353a0721a07ae2de6a58b277b89efa9a7b5dad
Arjun Yadav <arjun149133@gmail.com>
2025-01-22 17:17:00 +0530

    topbar

--- apps/excalidraw-fe/src/app/canvas/me/page.tsx
+++ apps/excalidraw-fe/src/app/canvas/me/page.tsx
@@ -1,11 +1,48 @@
-import Canvas from "@/components/canvas";
+import Canvas from "@/components/Canvas";
+import { Button } from "@/components/ui/button";
+import {
+  CircleIcon,
+  EraserIcon,
+  PenLineIcon,
+  RectangleHorizontalIcon,
+} from "lucide-react";
 
 const CanvasMePage = () => {
   return (
     <div className=" overflow-hidden">
+      <div className=" absolute top-2 left-5">
+        <Topbar />
+      </div>
       <Canvas />
     </div>
   );
 };
 
 export default CanvasMePage;
+
+const shapeButtons = [
+  {
+    icon: RectangleHorizontalIcon,
+  },
+  {
+    icon: CircleIcon,
+  },
+  {
+    icon: PenLineIcon,
+  },
+  {
+    icon: EraserIcon,
+  },
+];
+
+export function Topbar() {
+  return (
+    <div className=" bg-gray-700 rounded-lg">
+      {shapeButtons.map((s, index) => (
+        <Button key={index} className=" bg-gray-700 hover:bg-gray-600 ">
+          <s.icon />
+        </Button>
+      ))}
+    </div>
+  );
+}
