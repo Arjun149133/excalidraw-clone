4531a0e9660ca674207974bc5305fc0f1b5f6659
Arjun Yadav <arjun149133@gmail.com>
2025-01-22 16:38:20 +0530

    new fe

---
+++ apps/excalidraw-fe/src/app/canvas/me/page.tsx
@@ -0,0 +1,11 @@
+import Canvas from "@/components/canvas";
+
+const CanvasMePage = () => {
+  return (
+    <div className=" overflow-hidden">
+      <Canvas />
+    </div>
+  );
+};
+
+export default CanvasMePage;
---
+++ apps/excalidraw-fe/src/components/canvas.tsx
@@ -0,0 +1,105 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+
+type Shape = {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+};
+
+const Canvas = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [shapes, setShapes] = useState<Shape[]>([]);
+
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+
+    let startX = 0;
+    let startY = 0;
+    let clicked = false;
+
+    const handleResize = () => {
+      const canvas = canvasRef.current;
+      if (canvas) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+      }
+    };
+
+    const handleMouseDown = (e: MouseEvent) => {
+      startX = e.clientX;
+      startY = e.clientY;
+      clicked = true;
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+      if (clicked) {
+        const width = e.clientX - startX;
+        const hei = e.clientY - startY;
+
+        clear();
+        ctx.strokeStyle = "#ffffff";
+        ctx.strokeRect(startX, startY, width, hei);
+      }
+    };
+
+    const handleMouseUp = (e: MouseEvent) => {
+      clicked = false;
+      const w = e.clientX - startX;
+      const h = e.clientY - startY;
+
+      ctx.strokeStyle = "#ffffff";
+      ctx.strokeRect(startX, startY, w, h);
+
+      setShapes((s) => [
+        ...s,
+        {
+          x: startX,
+          y: startY,
+          w,
+          h,
+        },
+      ]);
+      console.log(shapes);
+    };
+
+    const handleMouseLeave = (e: MouseEvent) => {
+      //Todo
+      clicked = false;
+    };
+
+    const clear = () => {
+      ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+      ctx.fillStyle = "rgba(0, 0, 0)";
+      ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+      if (shapes) {
+        shapes.forEach((s) => {
+          ctx.strokeStyle = "#ffffff";
+          ctx.strokeRect(s.x, s.y, s.w, s.h);
+        });
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    canvasRef.current?.addEventListener("mousedown", handleMouseDown);
+    canvasRef.current?.addEventListener("mouseup", handleMouseUp);
+    canvasRef.current?.addEventListener("mousemove", handleMouseMove);
+    canvasRef.current?.addEventListener("mouseleave", handleMouseLeave);
+    clear();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      canvasRef.current?.removeEventListener("mousedown", handleMouseDown);
+      canvasRef.current?.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [canvasRef, shapes]);
+
+  return <canvas ref={canvasRef}></canvas>;
+};
+
+export default Canvas;
