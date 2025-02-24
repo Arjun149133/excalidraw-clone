bed3939c88b39e0ea6281488bf3db7ec4e540e10
Arjun Yadav <arjun149133@gmail.com>
2025-02-24 17:10:27 +0530

    prod urls

--- apps/excalidraw-fe/src/lib/config.ts
+++ apps/excalidraw-fe/src/lib/config.ts
@@ -1,5 +1,5 @@
-export const BACKEND_URL = "http://localhost:5000";
-export const WS_URL = "http://localhost:8080";
+// export const BACKEND_URL = "http://localhost:5000";
+// export const WS_URL = "http://localhost:8080";
 
-// export const BACKEND_URL = "https://collabdraw-backend-api.arjunyadav.website";
-// export const WS_URL = "https://collabdraw-websocket-api.arjunyadav.website";
+export const BACKEND_URL = "https://collabdraw-backend-api.arjunyadav.website";
+export const WS_URL = "https://collabdraw-websocket-api.arjunyadav.website";
